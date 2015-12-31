9e4636fe6ee006bbe61fcb29cd66076ae9e824ba
Winslow Dalpe <rwdalpe@gmail.com>
2015-12-30 23:45:46 -0800

    adding ability to xlink citetitle macro

--- lib/asciidoctor_extensions.rb
+++ lib/asciidoctor_extensions.rb
@@ -22,81 +22,85 @@ require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
 include ::Asciidoctor
 
 class GlossTermInlineMacro < Extensions::InlineMacroProcessor
-  use_dsl
+	use_dsl
 
-  named :glossterm
-  name_positional_attributes 'baseform', 'termtext', 'noindex'
+	named :glossterm
+	name_positional_attributes 'baseform', 'termtext', 'noindex'
 
-  def process parent, target, attrs
-    baseform = (attrs.key? 'baseform') ? attrs['baseform'] : nil
-    termtext = (attrs.key? 'termtext') ? attrs['termtext'] : baseform
-    noindex = (attrs.key? 'noindex') ? attrs['noindex'] == '1' : false
-    baseformAttr = baseform != nil ? %(baseform="#{baseform}") : ""
-    termtext = termtext != nil ? termtext : ""
-    if noindex
-      %(<glossterm #{baseformAttr}>#{termtext}</glossterm>)
-    else
-      %(<glossterm #{baseformAttr}>#{termtext}</glossterm><indexterm><primary>#{termtext}</primary></indexterm>)
-    end
-  end
+	def process parent, target, attrs
+		baseform = (attrs.key? 'baseform') ? attrs['baseform'] : nil
+		termtext = (attrs.key? 'termtext') ? attrs['termtext'] : baseform
+		noindex = (attrs.key? 'noindex') ? attrs['noindex'] == '1' : false
+		baseformAttr = baseform != nil ? %(baseform="#{baseform}") : ""
+		termtext = termtext != nil ? termtext : ""
+		if noindex
+			%(<glossterm #{baseformAttr}>#{termtext}</glossterm>)
+		else
+			%(<glossterm #{baseformAttr}>#{termtext}</glossterm><indexterm><primary>#{termtext}</primary></indexterm>)
+		end
+	end
 end
 
 class OlinkInlineMacro < Extensions::InlineMacroProcessor
-  use_dsl
+	use_dsl
 
-  named :olink
-  name_positional_attributes 'targetptr', 'text'
-  
-  def process parent, target, attrs
-    if attrs.key? 'text'
-      %(<olink targetdoc="#{target}" targetptr="#{attrs['targetptr']}">#{attrs['text']}</olink>)
-    else
-      %(<olink targetdoc="#{target}" targetptr="#{attrs['targetptr']}"/>)
-    end
-  end
+	named :olink
+	name_positional_attributes 'targetptr', 'text'
+	
+	def process parent, target, attrs
+		if attrs.key? 'text'
+			%(<olink targetdoc="#{target}" targetptr="#{attrs['targetptr']}">#{attrs['text']}</olink>)
+		else
+			%(<olink targetdoc="#{target}" targetptr="#{attrs['targetptr']}"/>)
+		end
+	end
 end
 
 class CiteTitleInlineMacro < Extensions::InlineMacroProcessor
-  use_dsl
+	use_dsl
 
-  named :citetitle
-  name_positional_attributes 'text'
-  
-  def process parent, target, attrs
-    %(<citetitle>#{attrs['text']}</citetitle>)
-  end
+	named :citetitle
+	name_positional_attributes 'text'
+	
+	def process parent, target, attrs
+		if target != nil and target != "" and target != ":"
+			%(<citetitle xlink:href="#{target}">#{attrs['text']}</citetitle>)
+		else
+			%(<citetitle>#{attrs['text']}</citetitle>)
+		end
+	end
 end
 
 class PersonInlineMacro < Extensions::InlineMacroProcessor
-  use_dsl
+	use_dsl
 
-  named :person
-  name_positional_attributes 'personname', 'format_str'
+	named :person
+	name_positional_attributes 'personname', 'format_str'
 
-  def process parent, target, attrs
-    if attrs.key? 'format_str'
-      format_str = attrs['format_str']
-      name_parts = attrs['personname'].split(' ')
-      
-      format_str = format_str.sub('{f}', '{0}')
-      format_str = format_str.sub('{l}', "{%d}" % [name_parts.length-1])
-      name_string = format_str
-      
-      for i in 0..name_parts.length-1
-        name_string = name_string.sub("{%d}" % [i], name_parts[i])
-      end
-      
-      name_string = name_string.sub('({[a-z]+}|{[0-9]+})', '')
-    else
-      name_string = attrs['personname']
-    end
-    %(<person><personname>#{name_string}</personname></person>)
-  end
+	def process parent, target, attrs
+		if attrs.key? 'format_str'
+			format_str = attrs['format_str']
+			name_parts = attrs['personname'].split(' ')
+			
+			format_str = format_str.sub('{f}', '{0}')
+			format_str = format_str.sub('{l}', "{%d}" % [name_parts.length-1])
+			name_string = format_str
+			
+			for i in 0..name_parts.length-1
+				name_string = name_string.sub("{%d}" % [i], name_parts[i])
+			end
+			
+			name_string = name_string.sub('({[a-z]+}|{[0-9]+})', '')
+		else
+			name_string = attrs['personname']
+		end
+		%(<person><personname>#{name_string}</personname></person>)
+	end
 end
 
 Extensions.register do
-  inline_macro GlossTermInlineMacro
-  inline_macro PersonInlineMacro
-  inline_macro OlinkInlineMacro
-  inline_macro CiteTitleInlineMacro
+	inline_macro GlossTermInlineMacro
+	inline_macro PersonInlineMacro
+	inline_macro OlinkInlineMacro
+	inline_macro CiteTitleInlineMacro
 end
