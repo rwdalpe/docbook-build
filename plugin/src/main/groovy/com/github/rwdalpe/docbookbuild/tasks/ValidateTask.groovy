317b20f5fe334f97fd881ff59c7c0f5d620d6fce
Winslow Dalpe <rwdalpe@gmail.com>
2016-01-17 20:54:16 -0800

    adding schematron validation after rnc validation

--- plugin/src/main/groovy/com/github/rwdalpe/docbookbuild/tasks/ValidateTask.groovy
+++ plugin/src/main/groovy/com/github/rwdalpe/docbookbuild/tasks/ValidateTask.groovy
@@ -1,6 +1,7 @@
 package com.github.rwdalpe.docbookbuild.tasks
 import com.github.rwdalpe.docbookbuild.DocbookBuildPlugin
 import com.thaiopensource.relaxng.jaxp.CompactSyntaxSchemaFactory
+import groovy.transform.PackageScope
 import org.apache.xerces.util.XMLCatalogResolver
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
@@ -19,10 +20,22 @@ public class ValidateTask extends DefaultTask {
     private File assetsDir = project.file("${project.docbookbuild.assetsExtractionDir}/${DocbookBuildPlugin.assetsDirName}")
     private File workingDir = project.docbookbuild.workingDir
     private File validationDir = project.file("${workingDir}/validation")
-    private final File schematronErrorsFile = project.file("${validationDir}/schematronErrors.txt")
 
+    File initialRncFile = project.file("${assetsDir}/docbook-5.0-extension_rwdalpe-rpg/rng/docbook-5.0-extension_rwdalpe-rpg.rnc")
     Set<File> srcFiles
     Set<File> catalogFiles
+
+    File getValidationDir() {
+        return validationDir
+    }
+
+    File getInitialRncFile() {
+        return initialRncFile
+    }
+
+    void setInitialRncFile(File initialRncFile) {
+        this.initialRncFile = initialRncFile
+    }
 
     Set<File> getCatalogFiles() {
         return catalogFiles
@@ -48,10 +61,40 @@ public class ValidateTask extends DefaultTask {
 
     private void validateFiles(Set<File> toValidate, File validationStylesheet) {
         rncValidateFiles(toValidate)
+        schematronValidateFiles(toValidate, validationStylesheet)
+    }
+
+    private void schematronValidateFiles(Set<File> toValidate, File schematronValidationStylesheet) {
+        XMLCatalogResolver resolver = new XMLCatalogResolver((String[])(catalogFiles.collect { it.absolutePath }))
+
+        System.setProperty('javax.xml.parsers.SAXParserFactory', 'org.apache.xerces.jaxp.SAXParserFactoryImpl')
+        System.setProperty('org.apache.xerces.xni.parser.XMLParserConfiguration', 'org.apache.xerces.parsers.XIncludeParserConfiguration')
+
+        TransformerFactory tFactory = TransformerFactory.newInstance()
+        Transformer t = tFactory.newTransformer(new StreamSource(schematronValidationStylesheet))
+
+        Set<File> errorsFiles = new HashSet<File>()
+
+        toValidate.each { f ->
+            def errorFile = new File("${getValidationDir().absolutePath}/${f}.schematronerrors".toString())
+            t.transform(new StreamSource(f),
+                    new StreamResult(errorFile))
+            errorsFiles.add(errorFile)
+        }
+
+        if(errorsFiles.any {it.length() > 0}) {
+            println("Schematron errors:")
+            errorsFiles.each { f ->
+                if(f.length() > 0) {
+                    println("\nErrors in ${f}: ")
+                    println(String.join("\n", f.readLines()))
+                }
+            }
+            throw new TaskExecutionException(this, new IllegalStateException("Schematron errors"))
+        }
     }
 
     private void rncValidateFiles(Set<File> toValidate) {
-        File validationRncFile = project.file("${assetsDir}/docbook-5.0-extension_rwdalpe-rpg/rng/docbook-5.0-extension_rwdalpe-rpg.rnc")
 
         XMLCatalogResolver resolver = new XMLCatalogResolver((String[])(catalogFiles.collect { it.absolutePath }))
 
@@ -59,7 +102,7 @@ public class ValidateTask extends DefaultTask {
         System.setProperty('org.apache.xerces.xni.parser.XMLParserConfiguration', 'org.apache.xerces.parsers.XIncludeParserConfiguration')
 
         SchemaFactory sFactory = CompactSyntaxSchemaFactory.newInstance()
-        Schema validatingSchema = sFactory.newSchema(validationRncFile)
+        Schema validatingSchema = sFactory.newSchema(initialRncFile)
 
         toValidate.each { f ->
             println "Validating ${f}"
@@ -83,16 +126,19 @@ public class ValidateTask extends DefaultTask {
             validationDir.mkdirs()
         }
 
+
         System.setProperty('javax.xml.parsers.SAXParserFactory', 'org.apache.xerces.jaxp.SAXParserFactoryImpl')
         System.setProperty('org.apache.xerces.xni.parser.XMLParserConfiguration', 'org.apache.xerces.parsers.XIncludeParserConfiguration')
 
-        def validationOutput = new File("${assetsDir}/schematron1.5/schematron-basic.xsl".toString())
+        def schematronCreationStylesheet = new File("${assetsDir}/schematron1.5/schematron-basic.xsl".toString())
+        def docbookSchematron = new File("${assetsDir}/docbook-5.0/sch/docbook.sch".toString())
+        def finalValidationStylesheet = new File("${validationDir}/schematronValidation.xsl".toString())
 
         TransformerFactory tFactory = TransformerFactory.newInstance()
-        Transformer t = tFactory.newTransformer(new StreamSource(validationOutput))
-        t.transform(new StreamSource(new File("${assetsDir}/docbook-5.0/sch/docbook.sch".toString())),
-                new StreamResult(new File("${validationDir}/schematronValidation.xsl".toString())))
+        Transformer t = tFactory.newTransformer(new StreamSource(schematronCreationStylesheet))
+        t.transform(new StreamSource(docbookSchematron),
+                new StreamResult(finalValidationStylesheet))
 
-        return validationOutput
+        return finalValidationStylesheet
     }
 }
